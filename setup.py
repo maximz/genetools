a8b6c0599ccd025a239f8dbd19a95bfa3a53a885
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2024-02-26 01:46:10 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/maximz/genetools",
-    version="0.7.0",
+    version="0.7.1",
     zip_safe=False,
     extras_require={
         # This will also install anndata and UMAP packages
